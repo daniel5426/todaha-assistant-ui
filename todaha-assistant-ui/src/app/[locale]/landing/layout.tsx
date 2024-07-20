eeecf6c50f8713e82f00004433a790ad89da6a0b
daniel benassayag <daniel.benassaya@natan.fr>
2024-07-21 00:59:26 +0300

    chore: Update ChatBot component layout and styles for improved UI and responsiveness

--- todaha-assistant-ui/src/app/[locale]/landing/layout.tsx
+++ todaha-assistant-ui/src/app/[locale]/landing/layout.tsx
@@ -20,12 +20,12 @@ export default function Layout({ children }: { children: any }) {
       {children}
       <ChatModal />
 
-      <div className="fixed bottom-5 end-5 z-10 " style={{ direction: isRTL ? "rtl" : "ltr" }}>
+      <div className="fixed bottom-5 start-5 z-10 " >
         <ThemeToggleButton
           shape="circle"
           color="ghost"
           className={
-            "border border-base-content/10 text-base-content/70 hover:bg-base-content/10"
+            "border border-base-content/10 text-base-content/70 hover:bg-base-content/10 bg-slate-500"
           }
         />
       </div>
--- todaha-assistant-ui/src/components/ThemeToggleButton.tsx
+++ todaha-assistant-ui/src/components/ThemeToggleButton.tsx
@@ -12,7 +12,7 @@ const ThemeToggleButton = (props: ButtonProps) => {
 
     return (
         <>
-            <Button {...props} onClick={() => changeTheme(state.theme == "dark" ? "light" : "dark")} className=" bg-sky-500">
+            <Button {...props} onClick={() => changeTheme(state.theme == "dark" ? "light" : "dark")} className={`bg-${state.theme == "dark" ? "slate-500" : "slate-300"}`}>
                 {state.theme == "light" && <Icon icon={sunIcon} fontSize={20} />}
                 {state.theme == "dark" && <Icon icon={moonIcon} fontSize={20} />}
             </Button>
