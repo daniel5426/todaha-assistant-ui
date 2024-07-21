91c07f628aa94646f0654e512bf0475d174cf536
daniel benassayag <daniel.benassaya@natan.fr>
2024-07-21 18:20:52 +0300

    chore: Update MessageList component to optimize rendering and fix SimpleBar import issue

--- todaha-assistant-ui/src/app/[locale]/admin/chat/components/MessageList.tsx
+++ todaha-assistant-ui/src/app/[locale]/admin/chat/components/MessageList.tsx
@@ -1,7 +1,6 @@
 "use client";
 import avatar1 from "@/assets/images/avatars/1.png";
 import { Key, forwardRef, useEffect, useRef } from "react";
-import SimpleBarCore from "simplebar-core";
 import dynamic from "next/dynamic";
 
 // SimpleBar import ChatList component
@@ -15,12 +14,14 @@ import {
   ChatBubbleTime,
 } from "@/components/daisyui";
 
+import React from "react";
 import { cn } from "@/helpers/utils/cn";
 import DateUtil from "@/helpers/utils/date";
 import { IChat, IChatMessage } from "@/types/apps/chat";
 import { useChat } from "../use-chat";
 import { containsHebrew } from "@/helpers/utils/string";
 import { useLocale, useTranslations } from "next-intl";
+import "simplebar/dist/simplebar.min.css";
 
 const SingleMessage = ({
   chat,
@@ -29,16 +30,17 @@ const SingleMessage = ({
   chat: IChat;
   message: IChatMessage;
 }) => {
+  const t = useTranslations("dashboard");
+  const locale = useLocale();
   const isHebrew = containsHebrew(message.message);
+  const isRTL = locale === "he";
+  const from_me = isRTL ? !message.from_me : message.from_me;
+
   if (!chat.messages) return null;
   const firstMessage = chat.messages[0];
   const date_started = DateUtil.formatted(firstMessage.send_at, {
     format: "hh:mm A",
   });
-  const t = useTranslations("dashboard");
-  const locale = useLocale();
-  const isRTL = locale === "he";
-  const from_me = isRTL ? !message.from_me : message.from_me;
 
   return (
     <div>
@@ -72,24 +74,22 @@ const SingleMessage = ({
 };
 
 const MessageList = () => {
-
   const { selectedChat } = useChat();
+  const t = useTranslations("dashboard");
   const locale = useLocale();
   const isRTL = locale === "he";
 
   if (!selectedChat) return null;
+
   const firstMessage = selectedChat.messages[0];
   const date_started = DateUtil.formatted(firstMessage.send_at, {
     format: "DD MMM hh:mm A",
   });
-  const t = useTranslations("dashboard");
   const msg_number = selectedChat.messages.length;
 
   return (
-    <Card className="bg-base-100" >
-      <div
-        className="flex items-center gap-3 p-3"
-      >
+    <Card className="bg-base-100">
+      <div className="flex items-center gap-3 p-3">
         <img src={selectedChat.image} className="size-10" alt="" />
         <div className="grow">
           <p className="text-base/none font-medium">{date_started}</p>
@@ -101,10 +101,7 @@ const MessageList = () => {
         </div>
       </div>
       <hr className="border-base-content/10" />
-      <SimpleBar
-        className="p-5"
-        style={{ height: "calc(100vh - 312px)" }}
-      >
+      <SimpleBar className="p-5" style={{ height: "calc(100vh - 312px)" }}>
         {selectedChat.messages.map(
           (message: IChatMessage, index: Key | null | undefined) => (
             <SingleMessage chat={selectedChat} message={message} key={index} />
